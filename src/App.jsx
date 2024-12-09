e52f4aaa9fbf72624c281a11fc5aa99fd5f03e26
Aswin <117593557+AswinM1@users.noreply.github.com>
2024-12-09 13:30:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+
+import './App.css'
+import About from './components/About'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Project from './components/Project'
+import Contact from './components/Contact'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div >
+        <Navbar></Navbar>
+     <Home></Home>
+     <About></About>
+     <Project></Project>
+     <Contact></Contact>
+
+     
+     
+   
+     
+ 
+       </div>
+    </>
+  )
+}
+
+export default App
